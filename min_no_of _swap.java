62873f1ed5e748f36cf5e60d7e73f556c06b4f74
chiranjit05 <46133445+chiranjit05@users.noreply.github.com>
2019-09-02 22:07:15 +0530

    2nd line println entered by user

--- min_no_of _swap.java
+++ min_no_of _swap.java
@@ -1,7 +1,8 @@
 class name
 {
 public static void main()
-{
-System.out.println("Hello world");
-}
-}
+  {
+    System.out.println("Hello world");
+    System.out.println("This is the second time accessing the code");
+  } // main function end
+}   // class end
